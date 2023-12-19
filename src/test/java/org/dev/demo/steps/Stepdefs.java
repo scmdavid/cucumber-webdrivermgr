977a571f3d35f4e7dd48b278db3a2d2a7cf96cff
Maven Chen <scmdavid@gmail.com>
2023-12-19 18:44:56 +0000

    ACreate chromedriver log

--- src/test/java/org/dev/demo/steps/Stepdefs.java
+++ src/test/java/org/dev/demo/steps/Stepdefs.java
@@ -9,12 +9,41 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Stepdefs {
     static ChromeDriver webDriver;
 
+    static private void initChromeLogging() {
+
+        final String rootPath = System.getProperty("user.dir");
+        final Path pathToLogDirectory = Path.of(rootPath, "target", "logs");
+        final Path pathToLog = Path.of(pathToLogDirectory.toAbsolutePath().toString(), "chromedriver.log");
+
+        try {
+            Files.createDirectories(pathToLogDirectory);
+            System.setProperty("webdriver.chrome.logfile", pathToLog.toAbsolutePath().toString());
+            System.setProperty("webdriver.chrome.verboseLogging", "true");
+
+            System.out.println("Setting log file to " + pathToLog.toAbsolutePath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @BeforeAll
     static public void initWebDriver() {
+        initChromeLogging();
         WebDriverManager.chromedriver().setup();
+    }
+
+    @AfterAll
+    static public void cleanUp() {
+        if (webDriver != null) {
+            webDriver.quit();
+        }
     }
 
     @Before
@@ -33,12 +62,5 @@ public class Stepdefs {
 
     @Then("browser is ready")
     public void browserIsReady() {
-    }
-
-    @AfterAll
-    static public void cleanUp(){
-        if (webDriver != null) {
-            webDriver.quit();
-        }
     }
 }
